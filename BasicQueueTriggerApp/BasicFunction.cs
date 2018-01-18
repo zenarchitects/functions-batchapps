a65f2fe7fae90f87dad906b3db7ea7189d520920
Kazuyuki Miyake <miyake@zenarc.jp>
2018-01-18 15:29:20 +0900

    Change Function Param

--- BasicQueueTriggerApp/BasicFunction.cs
+++ BasicQueueTriggerApp/BasicFunction.cs
@@ -7,7 +7,7 @@ namespace BasicQueueTriggerApp
     public static class BasicFunction
     {
         [FunctionName("BasicFunction")]
-        public static void Run([QueueTrigger("basic-queue-items", Connection = "")]string myQueueItem, TraceWriter log)
+        public static void Run([QueueTrigger("basic-queue-items")]string myQueueItem, TraceWriter log)
         {
             log.Info($"C# Queue trigger function processed: {myQueueItem}");
         }
