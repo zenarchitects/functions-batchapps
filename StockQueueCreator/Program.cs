81121aa368b397e92fa7e9ca95ff5c125b722b1b
Kazuyuki Miyake <miyake@zenarc.jp>
2018-01-23 00:03:53 +0900

    Added StockQueueCreator project

---
+++ StockQueueCreator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using CsvHelper;
+
+namespace StockQueueCreator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const string url = "https://functionsbatchapps01.blob.core.windows.net/sample/japan-all-stock-prices_20170104.csv";
+
+            Console.WriteLine("Start CSV Read");
+
+            var req = (HttpWebRequest)WebRequest.Create(url);
+            var resp = (HttpWebResponse)req.GetResponse();
+            var sr = new StreamReader(resp.GetResponseStream());
+
+            using (var csv = new CsvReader(sr))
+            {
+                csv.Configuration.RegisterClassMap<StockMapper>();
+                var stocks = csv.GetRecords<Stock>();
+                foreach (var p in stocks)
+                {
+                    Console.WriteLine($"Ticker: {p.Ticker}, Prive: {p.Price}");
+                }
+            }
+
+            sr.Close();
+
+            Console.WriteLine("Finish CSV Read");
+        }
+    }
+}
---
+++ StockQueueCreator/Stock.cs
@@ -0,0 +1,23 @@
+﻿using CsvHelper.Configuration;
+
+namespace StockQueueCreator
+{
+    public class Stock
+    {
+        public string Ticker { get; set; }
+        public string StockName { get; set; }
+        public string Price { get; set; }
+        public string Volume { get; set; }
+    }
+
+    public sealed class StockMapper : ClassMap<Stock>
+    {
+        public StockMapper()
+        {
+            Map(s => s.Ticker).Name("SC");
+            Map(s => s.StockName).Name("名称");
+            Map(s => s.Price).Name("前日終値");
+            Map(s => s.Volume).Name("出来高");
+        }
+    }
+}
