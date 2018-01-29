963c4024e5eb652b333be1e74b166844bbef63da
Kazuyuki Miyake <miyake@zenarc.jp>
2018-01-29 16:45:16 +0900

    Added JsonProperty

--- StockQueueCreator/Stock.cs
+++ StockQueueCreator/Stock.cs
@@ -1,12 +1,17 @@
 ﻿using CsvHelper.Configuration;
+using Newtonsoft.Json;
 
 namespace StockQueueCreator
 {
     public class Stock
     {
+        [JsonProperty(PropertyName = "ticker")]
         public string Ticker { get; set; }
+        [JsonProperty(PropertyName = "stockName")]
         public string StockName { get; set; }
+        [JsonProperty(PropertyName = "price")]
         public string Price { get; set; }
+        [JsonProperty(PropertyName = "volume")]
         public string Volume { get; set; }
     }
 
